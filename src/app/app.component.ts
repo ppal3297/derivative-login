e38ef16db8328b852010df8e67bb9f7945bf8dc3
Bigbuckbunny <pal.pankaj3297@gmail.com>
2020-10-02 12:39:06 +0530

    addded css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'firebaseAuth';
+export class AppComponent implements OnInit {
+  title = 'firebase-angular-auth';
+  isSignedIn = false;
+
+  constructor(public firebaseService: FirebaseService) {
+    // let lnk = document.getElementById('signup-link');
+    // lnk.addEventListener('click', (e: Event) => this.signupInitiate);
+  }
+  ngOnInit() {
+    if (localStorage.getItem('user') !== null) this.isSignedIn = true;
+    else this.isSignedIn = false;
+  }
+  async onSignup(email: string, password: string) {
+    await this.firebaseService.signup(email, password);
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+  }
+  async onSignin(email: string, password: string) {
+    await this.firebaseService.signin(email, password);
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
+  }
+  handleLogout() {
+    this.isSignedIn = false;
+  }
+  signupInitiate() {
+    document.getElementById('sign-in-div').classList.add('signin-div');
+    document.getElementById('sign-up-div').classList.remove('signup-div');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from 'src/environments/environment';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyADHXUCHN0l87-O9zkiu4n0pHDQ-Ki42B8',
+      authDomain: 'citi-login-ae617.firebaseapp.com',
+      databaseURL: 'https://citi-login-ae617.firebaseio.com',
+      projectId: 'citi-login-ae617',
+      storageBucket: 'citi-login-ae617.appspot.com',
+      messagingSenderId: '985767086509',
+      appId: '1:985767086509:web:c3c06bb129f10d3e243924',
+      measurementId: 'G-719F2CR6HQ',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FirebaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
